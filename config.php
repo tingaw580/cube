a88bf865c16a5f58c21985d73df1fa69afd353e4
tingaw580 <47096648+tingaw580@users.noreply.github.com>
2019-01-28 16:33:10 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<? php
+// android_id
+$ anid = " xxxxxxxxxxxxxxx " ;
+// tanda data
+$ sign = " xxxxxxxxxxxxxxx " ;
+// bantuan data jika tidak ada kosongkan
+$ aid = " xxxxxxxxxxxxxxx " ;
+// data uuid
+$ uuid = " xxxxxxxxxxxxxxx " ;
+? >
